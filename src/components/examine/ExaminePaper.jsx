ff472e0e5dd6a0728e48efd219d2d7b783295c7a
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-17 23:45:08 +0900

    create examin page skeleton code

--- src/components/examine/ExaminePaper.jsx
+++ src/components/examine/ExaminePaper.jsx
@@ -1,12 +1,14 @@
 import { PrevButton, NextButton } from "../common/Button";
 
-function ExaminePaper() {
-  return (
-    <div>
-          <PrevButton>이전</PrevButton>
-          <NextButton>다음</NextButton>
-    </div>
-  );
+function ExaminePaper({ title }) {
+    return (
+        <div className="examine-wrapper">
+            <h2>{title}</h2>
+            <article></article>
+            <PrevButton>이전</PrevButton>
+            <NextButton>다음</NextButton>
+        </div>
+    );
 }
 
 export default ExaminePaper;
