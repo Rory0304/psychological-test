259272121ad615d9d497e9af3744e872afbf00b7
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-18 21:58:44 +0900

    (FEAT) fetch api and create examine paper page

--- src/components/examine/ExaminePaper.jsx
+++ src/components/examine/ExaminePaper.jsx
@@ -1,14 +1,111 @@
-import { PrevButton, NextButton } from "../common/Button";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
-function ExaminePaper({ title }) {
+import { SET_LOADING, INPUT_ANSWER } from "../../reducer/variables";
+import { PrevButton, NextButton } from "../common/Button";
+import { LoadingPage } from "../common/LoadingPage";
+
+const QuestionArea = React.memo(function QuestionArea({ question, checked }) {
+    //01번, 02번이 질문 사항이고, 나머지(03 ~ 10)는 질문 용어에 대한 설명임.
+    const answers_num = ["01", "02"];
+    const qitemNo = question.qitemNo;
+    const dispatch = useDispatch();
+    // const pre_answers = useSelector(state => state.pre_answers);
+
     return (
-        <div className="examine-wrapper">
-            <h2>{title}</h2>
-            <article></article>
-            <PrevButton>이전</PrevButton>
-            <NextButton>다음</NextButton>
+        <div>
+            <p>
+                <span>{`Q${qitemNo}. `}</span>
+                {question.question}
+            </p>
+            {answers_num.map(
+                an =>
+                    question[`answer${an}`] && (
+                        <label key={`${qitemNo}${an}`}>
+                            {question[`answer${an}`]}
+                            <input
+                                type="radio"
+                                id={`${qitemNo}${an}`}
+                                name={qitemNo}
+                                value={question[`answerScore${an}`]}
+                                required
+                                onChange={e =>
+                                    dispatch({
+                                        type: INPUT_ANSWER,
+                                        payload: {
+                                            qitemNo: qitemNo,
+                                            answer: e.target.value
+                                        }
+                                    })
+                                }
+                            />
+                        </label>
+                    )
+            )}
         </div>
     );
+});
+
+function ExaminePaper({ title }) {
+    const dispatch = useDispatch();
+    const loading = useSelector(state => state.loading);
+    const [questions, setQuestion] = useState([]);
+    const limit = 5;
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            dispatch({ type: SET_LOADING, payload: true });
+            await axios
+                .get(
+                    `https://www.career.go.kr/inspct/openapi/test/questions?apikey=${process.env.REACT_APP_API_KEY}&q=6`
+                )
+                .then(data => {
+                    // [help] 데이터 수가 28개밖에 없어서 모두 fetch하는 방법으로 가져왔는데, 만약 데이터 수가 많아질 경우 잘라서 가져오는 방법 알아보기
+                    // offset과 limit를 활용하면 될 것 같은데 api 정보에는 없어서 어떤 식으로 가져오면 되는지 조언 필요
+                    console.log(data.data.RESULT);
+                    setQuestion([...data.data.RESULT]);
+                })
+                .catch(e => console.log(e));
+            dispatch({ type: SET_LOADING, payload: false });
+        };
+        fetchQuestions();
+    }, []);
+
+    if (loading && questions.length === 0) {
+        return (
+            <LoadingPage>
+                <p>Loading</p>
+            </LoadingPage>
+        );
+    } else {
+        return (
+            <div className="examine-wrapper">
+                <h2>{title}</h2>
+                <article>
+                    {questions.slice(offset, offset + limit).map(question => {
+                        return (
+                            <QuestionArea
+                                question={question}
+                                key={question.qitemNo}
+                            />
+                        );
+                    })}
+                    {offset !== 0 && (
+                        <PrevButton onClick={() => setOffset(offset - limit)}>
+                            이전
+                        </PrevButton>
+                    )}
+                    {offset !== questions.length - (questions.length % 5) && (
+                        <NextButton onClick={() => setOffset(offset + limit)}>
+                            다음
+                        </NextButton>
+                    )}
+                </article>
+            </div>
+        );
+    }
 }
 
 export default ExaminePaper;
