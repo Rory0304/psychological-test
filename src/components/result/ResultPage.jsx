a9319ac495f507cab6fcca5288cbea82370c4de7
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-26 19:26:02 +0900

    (REFACTOR) 그래프와 테이블 영역을 전체 페이지에서 컴포넌트  분리

--- src/components/result/ResultPage.jsx
+++ src/components/result/ResultPage.jsx
@@ -8,173 +8,16 @@ import {
     fetchJobDataByEducation,
     fetchJobDataByMajor
 } from "../../reducers/resultReducer";
+
+import { UserInfo, MajorTable, EduTable } from "./Table";
+import ValueGraph from "./ValueGraph";
 import { ViewResultButton, MoveToMainButton } from "../common/Button";
-
-import "./ResultPage.css";
-
-function UserInfo() {
-    const { name, gender, startDtm } = useSelector(state => state.qaData.answer_sheet);
-
-    return (
-        <ResultContent>
-            <table>
-                <caption className="visually-hidden">검사자 정보</caption>
-                <thead>
-                    <tr>
-                        <th scope="col">이름</th>
-                        <th scope="col">성별</th>
-                        <th scope="col">검사일</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{name}</td>
-                        <td>{gender === "100323" ? "남성" : "여성"}</td>
-                        <td>{new Date(startDtm).toLocaleDateString()}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </ResultContent>
-    );
-}
-function ValueGraph({ score_data }) {
-    const VerticalGraph = styled.div`
-        display: flex;
-        flex-direction: row-reverse;
-        height: 500px;
-        align-items: flex-end;
-        justify-content: space-around;
-        flex-direction: row;
-    `;
-
-    const VerticalBar = styled.div`
-        width: 12%;
-        position: relative;
-        background-color: #345bcd;
-        height: ${props => props.height};
-    `;
-
-    const BarLabel = styled.div`
-        width: 100%;
-        background-color: #eaeaea;
-        position: absolute;
-        bottom: 0;
-        text-align: center;
-    `;
-    return (
-        <ResultContent>
-            <h2>직업 가치관 결과</h2>
-            <VerticalGraph>
-                {score_data.map(
-                    (score, index) =>
-                        score.score && (
-                            <VerticalBar key={index} height={`${(score.score / 5) * 100}%`}>
-                                <BarLabel>{score.jobValue}</BarLabel>
-                            </VerticalBar>
-                        )
-                )}
-            </VerticalGraph>
-        </ResultContent>
-    );
-}
-
-function EduTable({ jobdata_edu }) {
-    return (
-        <section>
-            <h3>종사자 평균 학력별</h3>
-            <table>
-                <caption className="visually-hidden">종사자 평균 학력별 직업군</caption>
-                <thead>
-                    <tr>
-                        <th scope="col">분야</th>
-                        <th scope="col">직업</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {jobdata_edu.map(
-                        (ed, index) =>
-                            ed.jobs && (
-                                <tr key={index}>
-                                    <td>{ed.edu}</td>
-                                    <td>{ed.jobs}</td>
-                                </tr>
-                            )
-                    )}
-                </tbody>
-            </table>
-        </section>
-    );
-}
-
-function MajorTable({ jobdata_major }) {
-    return (
-        <section>
-            <h3>종사자 평균 전공별</h3>
-            <table>
-                <caption className="visually-hidden">종사자 평균 전공별 직업군</caption>
-                <thead>
-                    <tr>
-                        <th scope="col">분야</th>
-                        <th scope="col">직업</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {jobdata_major.map(
-                        (md, index) =>
-                            md.jobs && (
-                                <tr key={index}>
-                                    <td>{md.major}</td>
-                                    <td>{md.jobs}</td>
-                                </tr>
-                            )
-                    )}
-                </tbody>
-            </table>
-        </section>
-    );
-}
-function ResultPaper() {
-    const dispatch = useDispatch();
-    const { loading, score_data, jobdata_edu, jobdata_major } = useSelector(
-        state => state.resultData
-    );
-
-    useEffect(() => {
-        async function getPsychologicalResult() {
-            await dispatch(fetchScoreData());
-            dispatch(fetchJobDataByEducation());
-            dispatch(fetchJobDataByMajor());
-        }
-        getPsychologicalResult();
-    }, []);
-
-    if (loading && score_data && jobdata_edu && jobdata_major) {
-        <div>loading...</div>;
-    }
-
-    return (
-        <>
-            <hedaer>
-                <h1>직업 가치관 검사 결과표</h1>
-            </hedaer>
-            <main>
-                <UserInfo />
-                <ValueGraph score_data={score_data} />
-                <ResultContent>
-                    <h2>가치관과 관련이 높은 직업</h2>
-                    <EduTable jobdata_edu={jobdata_edu} />
-                    <MajorTable jobdata_major={jobdata_major} />
-                </ResultContent>
-            </main>
-        </>
-    );
-}
 
 function ResultPage() {
     const [viewStatus, setViewStatus] = useState(false);
 
     return (
-        <div className="result-wrapper">
+        <ResultWrapper>
             {viewStatus ? (
                 <>
                     <ResultPaper />
@@ -183,24 +26,95 @@ function ResultPage() {
                     </Link>
                 </>
             ) : (
-                <>
-                    <h1>검사가 완료되었습니다.</h1>
-                    <p>
-                        검사 결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게
-                        생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼
-                        기회를 제공합니다.
-                    </p>
-                    <ViewResultButton type="button" onClick={() => setViewStatus(true)}>
-                        결과 확인하기
-                    </ViewResultButton>
-                </>
+                <ResultPreview setViewStatus={setViewStatus} />
             )}
-        </div>
+        </ResultWrapper>
     );
 }
 
-export const ResultContent = styled.div`
-    margin-bottom: 20px;
+function ResultPreview({ setViewStatus }) {
+    return (
+        <>
+            <h1>검사가 완료되었습니다.</h1>
+            <p>
+                검사 결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게 생각하는지를
+                알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를 제공합니다.
+            </p>
+            <ViewResultButton type="button" onClick={() => setViewStatus(true)}>
+                결과 확인하기
+            </ViewResultButton>
+        </>
+    );
+}
+
+function ResultPaper() {
+    const dispatch = useDispatch();
+    const { loading, score_data, jobdata_edu, jobdata_major } = useSelector(
+        state => state.resultData
+    );
+    useEffect(() => {
+        dispatch(fetchScoreData()).then(() => {
+            dispatch(fetchJobDataByEducation());
+            dispatch(fetchJobDataByMajor());
+        });
+    }, []);
+
+    if (loading && (!score_data || !jobdata_edu || !jobdata_major)) {
+        <div>loading...</div>;
+    }
+
+    return (
+        <>
+            <header>
+                <h1>직업 가치관 검사 결과표</h1>
+                <p>
+                    직업가치관이란 직업을 선택할 때 영향을 끼치는 자신만의 믿음과 신념입니다. 따라서
+                    여러분의 직업생활과 관련하여 포기하지 않는 무게중심의 역할을 한다고 볼 수
+                    있습니다. 직업가치관검사는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를
+                    중요하게 생각하는지를 알려줍니다. 또 한 본인이 가장 중요하게 생각하는 가치를
+                    충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를 제공합니다.
+                </p>
+                <UserInfo />
+            </header>
+            <main>
+                <Section>
+                    <h2>1. 직업 가치관 결과</h2>
+                    <ValueGraph />
+                </Section>
+                <Section>
+                    <h2>2. 가치관과 관련이 높은 직업</h2>
+                    <EduTable />
+                    <MajorTable />
+                </Section>
+            </main>
+        </>
+    );
+}
+
+const ResultWrapper = styled.div`
+    width: calc(100% - 30px * 2);
+    padding: 40px;
+    margin: 0 auto;
+
+    & > header > h1 {
+        padding-bottom: 10px;
+        border-bottom: 1px solid black;
+        text-align: center;
+    }
+`;
+
+const Section = styled.section`
+    margin: 40px 0 65px 0;
+
+    & > h2 {
+        border-left: 12px solid#4F718F;
+        padding: 12px;
+        margin: 20px 0;
+    }
+
+    & > h3 {
+        margin: 20px 0;
+    }
 `;
 
 export default ResultPage;
