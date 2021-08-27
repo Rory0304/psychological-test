d03ef6692f89bc0341cf2e46467b7e407f9a5313
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-27 16:12:41 +0900

    (REFACTOR) 공통으로 사용되는 스타일 코드(폰트, 컬러, wrapper)를 묶어줌

---
+++ src/components/common/StyledConstants.js
@@ -0,0 +1,17 @@
+export const Colors = {
+    mainBlue: "#4f718f",
+    mainWhite: "#fff",
+    mainGray: "#F2F2F2",
+    mainGrayDarker: "#babcbe",
+    mainYellow: "#ffbf7c",
+    fontGray: "#707070",
+    shadowGray: "rgb(128 128 128 / 23%)"
+};
+
+export const FontSize = {
+    veryBig: "3.5rem",
+    big: "3rem",
+    middle1: "1.5rem",
+    middle2: "1.2rem",
+    small: "15px"
+};
