b8b6cf500e984139ee5f3de527c391a6792fc628
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-27 16:05:01 +0900

    (REFACTOR) 검사 페이지 컴포넌트 분리 - 질문 리스트, 페이지네이션

--- src/components/examine/ExaminePaper.jsx
+++ src/components/examine/ExaminePaper.jsx
@@ -1,104 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { ProgressBar } from "react-bootstrap";
 
-import { FormCheck, ProgressBar } from "react-bootstrap";
-import {
-    inputAnswer,
-    fetchQuestionData,
-    handleNextPage,
-    handlePrevPage
-} from "../../reducers/qaReducer";
-import { PrevButton, NextButton, NextButtonLabel, SubmitButton } from "../common/Button";
+import { fetchQuestionData } from "../../reducers/qaReducer";
+import QuestionList from "./QuestionList";
+import Pagination from "./Pagination";
 import { LoadingPage } from "../common/LoadingPage";
+import { ExamineWrapper, MainWrapper } from "../common/Wrapper";
 
 import "./ExaminePaper.css";
-
-const QuestionArea = React.memo(function QuestionArea({ question, setCount }) {
-    //01번, 02번이 질문 사항이고, 나머지(03 ~ 10)는 질문 용어에 대한 설명임.
-    const qitemNo = question.qitemNo;
-    const questionData = ["01", "02"].map(num => ({
-        key: qitemNo + num,
-        qitemNo: question.qitemNo,
-        answer: question[`answer${num}`],
-        answerScore: question[`answerScore${num}`],
-        id: qitemNo + num
-    }));
-
-    const dispatch = useDispatch();
-    const preAnswers = useSelector(state => state.qaData.pre_answers);
-
-    //이전 설문으로 돌아갈 경우, 이전의 기록을 불러와야 함.
-    const defaultAnswer = preAnswers.filter(pa => pa.qitemNo === qitemNo);
-    const defaultChecked = defaultAnswer[0] ? defaultAnswer[0].answer : false;
-
-    return (
-        <div className="question-list">
-            <p>
-                <span>{`Q${qitemNo}. `}</span>
-                {question.question}
-            </p>
-            <div className="answer-list">
-                {questionData.map(question => (
-                    <FormCheck
-                        className="radio-btn-area"
-                        type="radio"
-                        inline
-                        label={question.answer}
-                        id={question.id}
-                        key={question.id}
-                        name={question.qitemNo}
-                        checked={question.answerScore === defaultChecked}
-                        value={question.answerScore}
-                        required
-                        onChange={e => {
-                            if (!defaultChecked) {
-                                //해당 응답에 처음으로 체크를 했기 때문에 전체 응답 수를 1로 증가
-                                setCount(currentState => currentState + 1);
-                            }
-                            //이미 체크한 상태에서 체크 옵션만 바꾼 것이기 때문에 answer state만 변경
-                            dispatch(inputAnswer({ qitemNo, answer: e.target.value }));
-                        }}
-                    />
-                ))}
-            </div>
-        </div>
-    );
-});
-
-function Pagination({ count, questionDataLength }) {
-    const dispatch = useDispatch();
-    const { offset, limit } = useSelector(state => state.qaData.pagination_data);
-
-    return (
-        <div className="pagination-wrapper">
-            {offset !== 0 && (
-                <PrevButton onClick={() => dispatch(handleNextPage())}>이전</PrevButton>
-            )}
-            {offset !== questionDataLength - (questionDataLength % limit) ? (
-                <div class="next-button-area">
-                    <NextButtonLabel
-                        htmlFor="nextButton"
-                        status={count === offset || count % 5 !== 0}
-                    >
-                        모든 문항에 응답해야 합니다.
-                    </NextButtonLabel>
-                    <NextButton
-                        id="nextButton"
-                        disabled={count === offset || count % 5 !== 0}
-                        onClick={() => dispatch(handlePrevPage())}
-                    >
-                        다음
-                    </NextButton>
-                </div>
-            ) : (
-                <Link to="/result-view">
-                    <SubmitButton>검사 완료</SubmitButton>
-                </Link>
-            )}
-        </div>
-    );
-}
 
 function ExaminePaper() {
     const dispatch = useDispatch();
@@ -126,28 +36,27 @@ function ExaminePaper() {
         );
     }
     return (
-        <div className="main-wrapper">
-            <header>
-                <h2>
-                    검사 진행<span>{progress}%</span>
-                </h2>
-                <ProgressBar now={progress} visuallyhidden={true} />
-            </header>
-            <main>
-                <div className="questionArea-wrapper">
+        <MainWrapper>
+            <ExamineWrapper>
+                <header>
+                    <h2>
+                        검사 진행 <span>{progress}%</span>
+                    </h2>
+                    <ProgressBar now={progress} visuallyhidden={true} />
+                </header>
+                <main>
                     {questionData.slice(offset, offset + limit).map(question => (
-                        
-                        <QuestionArea
+                        <QuestionList
                             question={question}
                             key={question.qitemNo}
                             setCount={setCount}
                         />
-                    
                     ))}
+
                     <Pagination questionDataLength={questionData.length} count={count} />
-                </div>
-            </main>
-        </div>
+                </main>
+            </ExamineWrapper>
+        </MainWrapper>
     );
 }
 
