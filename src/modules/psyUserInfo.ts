38c07eb3a0aaac69e8293567b887aa4a86863a90
Rory <mitty0304@naver.com>
2023-08-27 20:47:01 +0900

    feat(psyUserInfo): add user info reducer

---
+++ src/modules/psyUserInfo.ts
@@ -0,0 +1,51 @@
+import { PsyUserInfoProps } from "../types/psyUserInfo";
+
+const INITIAL_STATE: PsyUserInfoProps = {
+    name: "",
+    gender: "male",
+    grade: "",
+    email: "",
+    startDtm: null
+};
+
+//
+// Action Type
+//
+const SET_USER_INFO = "psy_userInfo/SET_USER_INFO" as const;
+const RESET_USER_INFO = "psy_userInfo/RESET_USER_INFO" as const;
+
+//
+// Action Creator
+//
+export const setUserInfo = (
+    userInfo: Partial<Pick<PsyUserInfoProps, "name" | "gender" | "startDtm">>
+) => ({
+    type: SET_USER_INFO,
+    payload: userInfo
+});
+export const resetUserInfo = () => ({ type: RESET_USER_INFO });
+
+type PsyUserInfoType = ReturnType<typeof setUserInfo> | ReturnType<typeof resetUserInfo>;
+
+//
+//
+//
+const reducer = (state: PsyUserInfoProps = INITIAL_STATE, action: PsyUserInfoType) => {
+    switch (action.type) {
+        case SET_USER_INFO:
+            return {
+                ...state,
+                name: action.payload.name,
+                gender: action.payload.gender,
+                startDtm: action.payload.startDtm
+            };
+
+        case RESET_USER_INFO:
+            return INITIAL_STATE;
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
