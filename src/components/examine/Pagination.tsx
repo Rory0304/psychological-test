df02a68751bd872ae7feb9c48e2c6f26e33c03ef
Rory <mitty0304@naver.com>
2023-08-28 20:53:05 +0900

    style(Pagination): add styled prefix to pagination

--- src/components/examine/Pagination.tsx
+++ src/components/examine/Pagination.tsx
@@ -26,51 +26,54 @@ const Pagination: React.FC<PaginationProps> = ({
     const isAnsweredAllQuestions = answerCount === questionCount;
 
     return (
-        <PaginationWrapper>
-            <PrevButton onClick={onPrevClick} hidden={offset === 0}>
-                이전
-            </PrevButton>
-            <NextButtonArea>
+        <StyledPaginationWrapper>
+            <StyledButtonArea>
+                <StyledPrevButton onClick={onPrevClick} hidden={offset === 0}>
+                    이전
+                </StyledPrevButton>
+            </StyledButtonArea>
+            <StyledButtonArea>
                 {!isLastPage ? (
                     <>
-                        <NextButtonLabel htmlFor="nextButton" hidden={isAnsweredAllQuestions}>
+                        <StyledNextButtonLabel htmlFor="nextButton" hidden={isAnsweredAllQuestions}>
                             모든 문항에 응답해야 합니다.
-                        </NextButtonLabel>
-                        <NextButton
+                        </StyledNextButtonLabel>
+                        <StyledNextButton
                             id="nextButton"
                             disabled={!isAnsweredAllQuestions}
                             onClick={onNextClick}
                         >
                             다음
-                        </NextButton>
+                        </StyledNextButton>
                     </>
                 ) : (
                     <Link to="/result-view">
-                        <NextButton id="nextButton" disabled={!isAnsweredAllQuestions}>
+                        <StyledNextButton id="nextButton" disabled={!isAnsweredAllQuestions}>
                             검사 완료
-                        </NextButton>
+                        </StyledNextButton>
                     </Link>
                 )}
-            </NextButtonArea>
-        </PaginationWrapper>
+            </StyledButtonArea>
+        </StyledPaginationWrapper>
     );
 };
 
-const NextButtonArea = styled.div`
+const StyledPaginationWrapper = styled.div`
+    text-align: center;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    margin: 3rem 0;
+`;
+
+const StyledButtonArea = styled.div`
     width: 47%;
     text-align: center;
     display: inline-block;
 `;
 
-const PaginationWrapper = styled.div`
-    text-align: center;
-    display: flex;
-    align-items: flex-end;
-    justify-content: space-between;
-`;
-
-const PrevButton = styled.button<{ hidden: boolean }>`
-    width: 47%;
+const StyledPrevButton = styled.button<{ hidden: boolean }>`
+    width: 100%;
     height: 50px;
     border: none;
     padding: 8px 15px;
@@ -85,7 +88,7 @@ const PrevButton = styled.button<{ hidden: boolean }>`
     }
 `;
 
-const NextButton = styled.button`
+const StyledNextButton = styled.button`
     width: 100%;
     height: 50px;
     border: none;
@@ -105,7 +108,7 @@ const NextButton = styled.button`
     }
 `;
 
-const NextButtonLabel = styled.label<{ hidden: boolean }>`
+const StyledNextButtonLabel = styled.label<{ hidden: boolean }>`
     width: 100%;
     font-size: ${Typography.small};
     visibility: ${props => (props.hidden ? "hidden" : "visible")};
