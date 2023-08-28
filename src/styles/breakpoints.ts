fbb1171d1be4c4c7e598429eec2549f5bd89c0eb
Rory <mitty0304@naver.com>
2023-08-28 20:53:47 +0900

    feat(breakpoints): add breakpoints specification

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,11 @@
+const laptopSmaller = "only screen and (max-width:1024px)";
+const tabletSmaller = "only screen and (max-width:768px)";
+const mobileSmaller = "only screen and (max-width:640px)";
+
+const breakpoints = Object.freeze({
+    laptopSmaller,
+    tabletSmaller,
+    mobileSmaller
+});
+
+export default breakpoints;
