a3ee440cf606f1a685cd23d6bc51a1b01571a427
Rory <mitty0304@naver.com>
2023-09-01 17:01:49 +0900

    style(ResultPage): add Styled prefix

--- src/components/result/ResultPage.tsx
+++ src/components/result/ResultPage.tsx
@@ -23,12 +23,12 @@ const ResultPage: React.FC = () => {
             {resultView === "preview" ? (
                 <PreviewPage onResultPageShow={handleResultPageShow} />
             ) : (
-                <ResultWrapper>
+                <StyledResultWrapper>
                     <ResultPaper />
                     <Link to="/" className="goto-main">
-                        <MoveToMainButton>다시 검사하기</MoveToMainButton>
+                        <StyledMoveToMainButton>다시 검사하기</StyledMoveToMainButton>
                     </Link>
-                </ResultWrapper>
+                </StyledResultWrapper>
             )}
         </>
     );
@@ -56,7 +56,7 @@ const ResultPaper: React.FC = () => {
     }
     return (
         <>
-            <ResultHeader>
+            <StyledResultHeader>
                 <h1>직업 가치관 검사 결과표</h1>
                 <p>
                     직업가치관이란 직업을 선택할 때 영향을 끼치는 자신만의 믿음과 신념입니다. 따라서
@@ -66,29 +66,29 @@ const ResultPaper: React.FC = () => {
                     충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를 제공합니다.
                 </p>
                 <UserInfoTable name={name} gender={gender} startDtm={startDtm} />
-            </ResultHeader>
+            </StyledResultHeader>
             <main>
-                <Section>
+                <StyledSection>
                     <h2>1. 직업 가치관 결과</h2>
                     <ValueGraph />
-                </Section>
-                <Section>
+                </StyledSection>
+                <StyledSection>
                     <h2>2. 가치관과 관련이 높은 직업</h2>
                     <EduTable jobDataEdu={jobdata_edu} />
                     <MajorTable jobDataMajor={jobdata_major} />
-                </Section>
+                </StyledSection>
             </main>
         </>
     );
 };
 
-const ResultWrapper = styled.div`
+const StyledResultWrapper = styled.div`
     width: 75%;
     padding: 60px 0;
     margin: 0 auto;
 `;
 
-const ResultHeader = styled.header`
+const StyledResultHeader = styled.header`
     & > h1 {
         width: fit-content;
         margin: 0 auto;
@@ -107,7 +107,7 @@ const ResultHeader = styled.header`
     }
 `;
 
-const Section = styled.section`
+const StyledSection = styled.section`
     margin: 40px 0 65px 0;
 
     & > h2 {
@@ -124,7 +124,7 @@ const Section = styled.section`
     }
 `;
 
-const MoveToMainButton = styled.span`
+const StyledMoveToMainButton = styled.span`
     width: 50%;
     background-color: ${Colors.mainBlue};
     color: ${Colors.mainWhite};
