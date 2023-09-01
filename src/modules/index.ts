631f35c07124f3ca1d8b698e7e12667ae95f02b4
Rory <mitty0304@naver.com>
2023-09-01 17:02:05 +0900

    chore: delete psyQuestion

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux";
 
 import psyUserInfo from "./psyUserInfo";
-import psyQuestion from "./psyQuestion";
 import psyAnswerSheet from "./psyAnswerSheet";
 import psyResult from "./psyResult";
 
 const rootReducer = combineReducers({
     psyUserInfo,
-    psyQuestion,
     psyAnswerSheet,
     psyResult
 });
